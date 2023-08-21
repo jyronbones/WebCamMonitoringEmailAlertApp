cccde89ffab7f164e60ea5ff78a81ed3fb8cebe3
Byron <byronjones77@gmail.com>
2023-08-20 20:18:47 -0400

    update comments

--- emailing.py
+++ emailing.py
@@ -3,10 +3,8 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Set your SendinBlue API key here
 SMTP_KEY = os.getenv("SMTP_KEY")
 
 
@@ -15,7 +13,6 @@ def send_email():
     receiver_email = os.getenv("EMAIL_RECEIVER")
     subject = "Motion Detected"
 
-    # Get the current timestamp
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     sendinblue_url = "https://api.sendinblue.com/v3/smtp/email"
@@ -24,7 +21,6 @@ def send_email():
         "Content-Type": "application/json",
     }
 
-    # Create the email content
     email_content = {
         "sender": {"name": "Motion Detection App", "email": sender_email},
         "to": [{"email": receiver_email}],
@@ -32,7 +28,6 @@ def send_email():
         "htmlContent": f"<p>Motion detected at: {timestamp}</p>",
     }
 
-    # Send the email using SendinBlue API
     try:
         response = requests.post(sendinblue_url, json=email_content, headers=headers)
         response.raise_for_status()
